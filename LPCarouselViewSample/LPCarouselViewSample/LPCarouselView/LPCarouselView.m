f90d9e70c42e791df1cb2b624135d62eed990382
litt1e-p <free9fw@gmail.com>
2016-02-01 10:36:43 +0800

    bugfix

--- LPCarouselViewSample/LPCarouselViewSample/LPCarouselView/LPCarouselView.m
+++ LPCarouselViewSample/LPCarouselViewSample/LPCarouselView/LPCarouselView.m
@@ -8,7 +8,7 @@
 
 #import "LPCarouselView.h"
 #import "LPCarouselViewCell.h"
-#import <UIImageView+WebCache.h>
+#import "UIImageView+WebCache.h"
 
 @interface LPCarouselView()<UICollectionViewDataSource, UICollectionViewDelegate>
 
